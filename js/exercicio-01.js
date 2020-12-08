d2cbccb78e0c88b6574913d5aab949cb12074788
Júlia Aparecida <julia.aparecida25@gmail.com>
2020-12-07 22:18:57 -0300

    Exercícico 1

---
+++ js/exercicio-01.js
@@ -0,0 +1,31 @@
+function Exemplo(){
+// document.write("Escrevendo na tela");
+
+    document.getElementById("mensagem").innerHTML = "<b>Escrevendo na tela</b>";
+
+    document.getElementById("numero").value = "teste";
+    
+    console.log(document.getElementById("mensagem").innerText);
+
+    let numero = document.getElementById("numero").value;
+    console.log(numero);
+}
+
+function Trocar(){
+    let numero = document.getElementById("numero").value;
+
+    if(numero<20){//teste
+        //resposta verdadeira
+        document.getElementById("mensagem").innerText = "Menor que 20";
+    }
+    else if(numero == 20)
+    {
+        document.getElementById("mensagem").innerText = "Igual a 20";
+    }
+    else
+    {
+        //resposta falsa
+        document.getElementById("mensagem").innerText = "Maior que 20";
+
+    }
+}
---
+++ js/funcoes.js
@@ -0,0 +1,38 @@
+console.log("Moreira Julia")
+
+/*Boas práticas para nome de variáveis
+ - não utilizar caracteres especiais
+ - não começar com números
+ - não utilizar espaços
+ - ter nome significativo
+ - manter nome em minúsculo
+ - quando for palavra composta, colocar as inciais a partir da segunda palavra em maiúsculo
+ - o js diferencia minúsculas e MAIÚSCULAS (case sensitive) 
+ - o js é fracamente tipado
+ texto "Jake"
+ numérico 10
+    inteiro 10
+    decimal 10.5
+ lógico/booleano --> true / false --> 1 / 0
+    10 > 7 -> true
+    tipos de dados básicos do javascript são:
+    - string --> "texto"
+    - number --> 10 ou 10.5
+    - boolean --> true / false
+    = --> atribuição
+    + --> concatenação quando é com texto
+    + --> adição quando é só com números
+ */
+
+ /*let nome = prompt("Informe seu nome");
+ alert("Olá " + nome + " seja bem vindo!");*/
+
+ /*let numero1 = prompt("Digite um número");
+ let numero2 = prompt("Digite outro número");
+
+ alert(numero1 * numero2);*/
+
+ let nome = prompt("Digite seu nome");
+ let idade = prompt("Informe sua idade");
+
+ alert(nome + " tem " + idade + " anos de idade"); 
